01104c25e3d5ec63c1bae3437f9473d203d872b3
Your Name <info@aglowiditsolutions.com>
2019-01-04 15:41:39 +0530

    093 Auxiliary, 092 Understanding Higher Order Components (HOCs)

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
+import WithClass from '../hoc/withClass';
 // import Person from '../components/Persons/Person/Person';
 
 // import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
@@ -96,7 +97,7 @@ class App extends PureComponent {
 
  
     return (
-        <div className={classes.App}>
+        <WithClass classes={classes.App}>
           <button onClick={() => this.setState({showPerson: true })} > Show Persons </button>
 	        <Cockpit 
             appTitle={this.props.title}
@@ -104,7 +105,7 @@ class App extends PureComponent {
             persons={this.state.persons} 
             clicked={this.togglePersonHandler} />
 	        {persons}	
-	      </div>
+	      </WithClass>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Sanjay Here'))
   }
